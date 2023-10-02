f1c5b6203b72b4d20f4efce3e802c99221cb63fc
WillyWilliam00 <133374335+WillyWilliam00@users.noreply.github.com>
2023-10-02 14:05:25 +0000

    up

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
+let DropChoise 
+let result = []
+const row = document.querySelector("tbody")
+
+table() // funzione asicrona per far apparire la tabella 
+
+document.querySelectorAll('li').forEach( function(li) { //inserisco event listener per modificare il titolo del drop e prendere la viaribile da filtrare
+            
+    li.addEventListener('click', function() {
+        document.querySelector('.dropdown-toggle').innerText = li.innerText;
+        DropChoise = `${li.innerText.toLowerCase()}`
+        text() //richiamo la funzione text per modificare la tabella nel caso si volesse cambiare il filtro e l'input serach fosse pieno
+        
+    });
+});
+
+function text() { //funzione che aggiorna il dom in base all'input value search
+    
+    const value = document.querySelector("input").value
+
+    if(DropChoise === undefined){
+        alert("Inserisci un filtro!")
+        
+    } else {
+        
+        let ArrayUsersByDrop = result.filter(user => 
+            user[DropChoise].toLowerCase().includes(value.toLowerCase())
+        )
+        
+        row.innerHTML =  ArrayUsersByDrop.map(user => 
+            
+            `<tr>
+                    <th scope="user">${user["id"]}</th>
+                    <td>${user["email"]}</td>
+                    <td>${user["username"]}</td>
+                    <td>${user["name"]}</td>
+             </tr>` 
+        ).join("")
+        if(ArrayUsersByDrop.length === 0){
+            row.innerHTML = "INSERISCI UN PARAMETRO VALIDO"
+        }
+    }
+}
+
+
+async function table() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const data = await response.json()
+    data.forEach(user => {
+        result.push(user)
+    })
+    row.innerHTML =  data.map(user => 
+        
+        `<tr>
+                <th scope="user">${user["id"]}</th>
+                <td>${user["email"]}</td>
+                <td>${user["username"]}</td>
+                <td>${user["name"]}</td>
+         </tr>` 
+    ).join("")
+    return data
+    
+}
+
+
 
